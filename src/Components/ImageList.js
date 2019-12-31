75a47103c72273bc626a46ce01daf79ed1e56cf3
Nijar <nijarr2020@gmail.com>
2019-12-31 15:05:09 +0530

    search bar size changeed

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -5,21 +5,25 @@ class ImageList extends Component {
     const { images } = this.props;
     const List = images.length ? (
       images.map(image => {
+        console.log('image :',image)
         return (
-          <div key={image.id}>
+          <div  key={image.id}>
             <div className='images'>
-              <img
-                className='img-fluid img-thumbnail'
-                alt={image.alt_description}
-                key={image.id}
-                src={image.urls.regular}
-              />
+               <div style={{textAlign:'center'}}>
+                <img
+                  className='img-fluid img-thumbnail'
+                  alt={image.alt_description}
+                  key={image.id}
+                  src={image.urls.regular}
+                />
+              </div>
             </div>
           </div>
         )
       })
     ) : (
-        <div className='no-image' style={{ width: '450px', margin: 'auto' }}>
+        <div className='no-image'
+             style={{ width: '450px', margin: 'auto' }}>
           Use different key words...
       </div>
       )
